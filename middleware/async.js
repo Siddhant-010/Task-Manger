327169c2d1aa2866082b4d750d29d77a9e613a73
Siddhant Jain <115080372+Siddhant-010@users.noreply.github.com>
2022-11-10 13:01:23 +0530

    Add files via upload

---
+++ middleware/async.js
@@ -0,0 +1,11 @@
+const asyncWrapper = (fn) => {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error); // passing it to next middleware
+    }
+  };
+};
+
+module.exports = asyncWrapper;
